b81751a2652cd873acecbfa29fa41b0f17b260a9
Treanor <d26553@eon.com>
2022-11-30 13:34:05 +0100

    second commit

---
+++ src/main/java/com/eon/jdk19loom/Jdk19LoomApplication.java
@@ -0,0 +1,13 @@
+package com.eon.jdk19loom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk19LoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdk19LoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eon/jdk19loom/Jdk19LoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eon.jdk19loom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk19LoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
