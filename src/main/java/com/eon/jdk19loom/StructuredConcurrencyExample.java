1d40d9066ace751e3d84dcc25ae101328cd6d72a
Treanor <d26553@eon.com>
2022-12-01 14:20:18 +0100

    final commit, before presentation

--- src/main/java/com/eon/jdk19loom/StructuredConcurrencyExample.java
+++ src/main/java/com/eon/jdk19loom/StructuredConcurrencyExample.java
@@ -34,69 +34,69 @@ public final class StructuredConcurrencyExample extends AbstractLoomExample {
 	public static final String EBOOK = "ebook.pdf";
 
 
-	protected String findPageContainingWordLike(String word) throws IOException {
-			StringBuilder bookContent = parsePdf(EBOOK);
-			String[] words = bookContent.toString().split(" ");
-			String homonym = Arrays.stream(words).filter(w->soundSame(w,word)).findFirst().get();
-			int startingIndex = bookContent.toString().indexOf(homonym);
-			String subBook = bookContent.toString().substring(0,startingIndex);
-			int indexOfPageNumber = subBook.lastIndexOf(PAGE_MARKER);
-				System.out.println("here: "+homonym+". on page: "+subBook.substring(indexOfPageNumber+2,indexOfPageNumber+5));
-
-				return "here: "+homonym+". on page: "+subBook.substring(indexOfPageNumber+2,indexOfPageNumber+5);
-	}
-
-	protected List<TextExtractionStrategy> parsePdfConcurrent(String pdf) throws IOException {
-		StringBuilder builder=new StringBuilder();
-		List<TextExtractionStrategy> pages;
-		PdfReader reader = new PdfReader(pdf);
-		PdfReaderContentParser parser = new PdfReaderContentParser(reader);
-		TextExtractionStrategy strategy;
-		pages = new ArrayList<>();
-		for (int i = 1; i <= reader.getNumberOfPages(); i++)
-			pages.add(parser.processContent(i, new SimpleTextExtractionStrategy()));
-
-
-		reader.close();
-		return pages;
-	}
-	protected StringBuilder parsePdf(String pdf) throws IOException {
-		StringBuilder builder=new StringBuilder();
-		PdfReader reader = new PdfReader(pdf);
-		PdfReaderContentParser parser = new PdfReaderContentParser(reader);
-		TextExtractionStrategy strategy;
-		for (int i = 1; i <= reader.getNumberOfPages(); i++) {
-			strategy = parser.processContent(i, new SimpleTextExtractionStrategy());
-			builder.append(strategy.getResultantText());
-			builder.append(PAGE_MARKER +i);
-		}
-		reader.close();
-		return builder;
-	}
-
-	protected boolean soundSame(String wordA, String wordB){
-		String scorew1 =Soundex.getGode(wordA);
-		String scorew2 =Soundex.getGode(wordB);
-
-		return scorew1.equals(scorew2);
-	}
-	protected boolean soundSimilar(String wordA, String wordB){
-		String scorew1 =Soundex.getGode(wordA);
-		String scorew2 =Soundex.getGode(wordB);
-		int w1=Integer.valueOf(scorew1.substring(1,scorew1.length()));
-		int w2=Integer.valueOf(scorew2.substring(1,scorew2.length()));
-		return w1==w2;
-	}
-
-	protected boolean soundSimilarWithTolerance(String wordA, String wordB, int tolerance) throws IOException {
-
-		String scorew1 =Soundex.getGode(wordA);
-		String scorew2 =Soundex.getGode(wordB);
-		int w1=Integer.valueOf(scorew1.substring(1,scorew1.length()));
-		int w2=Integer.valueOf(scorew2.substring(1,scorew2.length()));
-		return !(Math.abs( w1-w2) > tolerance);
-	}
-
+//	protected String findPageContainingWordLike(String word) throws IOException {
+//			StringBuilder bookContent = parsePdf(EBOOK);
+//			String[] words = bookContent.toString().split(" ");
+//			String homonym = Arrays.stream(words).filter(w->soundSame(w,word)).findFirst().get();
+//			int startingIndex = bookContent.toString().indexOf(homonym);
+//			String subBook = bookContent.toString().substring(0,startingIndex);
+//			int indexOfPageNumber = subBook.lastIndexOf(PAGE_MARKER);
+//				System.out.println("here: "+homonym+". on page: "+subBook.substring(indexOfPageNumber+2,indexOfPageNumber+5));
+//
+//				return "here: "+homonym+". on page: "+subBook.substring(indexOfPageNumber+2,indexOfPageNumber+5);
+//	}
+//
+//	protected List<TextExtractionStrategy> parsePdfConcurrent(String pdf) throws IOException {
+//		StringBuilder builder=new StringBuilder();
+//		List<TextExtractionStrategy> pages;
+//		PdfReader reader = new PdfReader(pdf);
+//		PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+//		TextExtractionStrategy strategy;
+//		pages = new ArrayList<>();
+//		for (int i = 1; i <= reader.getNumberOfPages(); i++)
+//			pages.add(parser.processContent(i, new SimpleTextExtractionStrategy()));
+//
+//
+//		reader.close();
+//		return pages;
+//	}
+//	protected StringBuilder parsePdf(String pdf) throws IOException {
+//		StringBuilder builder=new StringBuilder();
+//		PdfReader reader = new PdfReader(pdf);
+//		PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+//		TextExtractionStrategy strategy;
+//		for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+//			strategy = parser.processContent(i, new SimpleTextExtractionStrategy());
+//			builder.append(strategy.getResultantText());
+//			builder.append(PAGE_MARKER +i);
+//		}
+//		reader.close();
+//		return builder;
+//	}
+//
+//	protected boolean soundSame(String wordA, String wordB){
+//		String scorew1 =Soundex.getGode(wordA);
+//		String scorew2 =Soundex.getGode(wordB);
+//
+//		return scorew1.equals(scorew2);
+//	}
+//	protected boolean soundSimilar(String wordA, String wordB){
+//		String scorew1 =Soundex.getGode(wordA);
+//		String scorew2 =Soundex.getGode(wordB);
+//		int w1=Integer.valueOf(scorew1.substring(1,scorew1.length()));
+//		int w2=Integer.valueOf(scorew2.substring(1,scorew2.length()));
+//		return w1==w2;
+//	}
+//
+//	protected boolean soundSimilarWithTolerance(String wordA, String wordB, int tolerance) throws IOException {
+//
+//		String scorew1 =Soundex.getGode(wordA);
+//		String scorew2 =Soundex.getGode(wordB);
+//		int w1=Integer.valueOf(scorew1.substring(1,scorew1.length()));
+//		int w2=Integer.valueOf(scorew2.substring(1,scorew2.length()));
+//		return !(Math.abs( w1-w2) > tolerance);
+//	}
+//
 
 	protected String getStringInAConvolutedWay() throws InterruptedException, ExecutionException {
 		try (var scope = new StructuredTaskScope.ShutdownOnSuccess<String>()) {
@@ -104,7 +104,7 @@ public final class StructuredConcurrencyExample extends AbstractLoomExample {
 				 scope.fork(() -> getHello());
 			}
 
-		    scope.join();
+		    scope.join(); //blocking
 
 		    return scope.result();
 		}
--- src/test/java/com/eon/jdk19loom/StructuredConcurrencyExampleTest.java
+++ src/test/java/com/eon/jdk19loom/StructuredConcurrencyExampleTest.java
@@ -17,36 +17,36 @@ class StructuredConcurrencyExampleTest {
 		String hello = new StructuredConcurrencyExample().getStringInAConvolutedWay();
 		System.out.println(hello+", world");
 	}
-	@Test
-	void testSoundexDemo(){
-		assertTrue(new StructuredConcurrencyExample().soundSame("byte","bite"));
-		assertTrue(new StructuredConcurrencyExample().soundSame("reid","reed"));
-		assertTrue(new StructuredConcurrencyExample().soundSame("jik","jick"));
-	}
-
-	@Test
-	void testSoundexDemo2(){
-		assertTrue(new StructuredConcurrencyExample().soundSimilar("byte","bite"));
-		assertTrue(new StructuredConcurrencyExample().soundSimilar("reid","reed"));
-		assertTrue(new StructuredConcurrencyExample().soundSimilar("jik","jick"));
-		assertTrue(new StructuredConcurrencyExample().soundSimilar("home","ome"));
-	}
-
-	@Test
-	void testSoundexDemo4() throws IOException {
-		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("byte","bite",0));
-		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("reid","reed",0));
-		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("jik","jick",0));
-		assertFalse(new StructuredConcurrencyExample().soundSimilarWithTolerance("home","ute",10));
-	}
-
-	@Test
-	void findWordLike() throws IOException {
-		StopWatch stopWatch=new StopWatch();
-		stopWatch.start();
-		String homonym = new StructuredConcurrencyExample().findPageContainingWordLike(DECLAN);
-		stopWatch.stop();
-		System.out.println(homonym.substring(homonym.indexOf("here:")+6,homonym.indexOf(". on "))+" is not really like "+DECLAN+", in "+stopWatch.prettyPrint());
-
-	}
+//	@Test
+//	void testSoundexDemo(){
+//		assertTrue(new StructuredConcurrencyExample().soundSame("byte","bite"));
+//		assertTrue(new StructuredConcurrencyExample().soundSame("reid","reed"));
+//		assertTrue(new StructuredConcurrencyExample().soundSame("jik","jick"));
+//	}
+//
+//	@Test
+//	void testSoundexDemo2(){
+//		assertTrue(new StructuredConcurrencyExample().soundSimilar("byte","bite"));
+//		assertTrue(new StructuredConcurrencyExample().soundSimilar("reid","reed"));
+//		assertTrue(new StructuredConcurrencyExample().soundSimilar("jik","jick"));
+//		assertTrue(new StructuredConcurrencyExample().soundSimilar("home","ome"));
+//	}
+//
+//	@Test
+//	void testSoundexDemo4() throws IOException {
+//		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("byte","bite",0));
+//		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("reid","reed",0));
+//		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("jik","jick",0));
+//		assertFalse(new StructuredConcurrencyExample().soundSimilarWithTolerance("home","ute",10));
+//	}
+//
+//	@Test
+//	void findWordLike() throws IOException {
+//		StopWatch stopWatch=new StopWatch();
+//		stopWatch.start();
+//		String homonym = new StructuredConcurrencyExample().findPageContainingWordLike(DECLAN);
+//		stopWatch.stop();
+//		System.out.println(homonym.substring(homonym.indexOf("here:")+6,homonym.indexOf(". on "))+" is not really like "+DECLAN+", in "+stopWatch.prettyPrint());
+//
+//	}
 }
