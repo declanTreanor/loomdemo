a206be548b056c80fd9efe2fa139750474e466e6
Treanor <d26553@eon.com>
2022-12-01 09:36:17 +0100

    added soundex implementation

--- src/main/java/com/eon/jdk19loom/StructuredConcurrencyExample.java
+++ src/main/java/com/eon/jdk19loom/StructuredConcurrencyExample.java
@@ -11,27 +11,35 @@ public final class StructuredConcurrencyExample extends AbstractLoomExample {
 	private synchronized void incrementCounter(){
 		synchronizedCounter++;
 	}
-	public class Response {
 
-	}
-
-	public class UserInterests {
-
-	}
-
-	public class SubscriptionTier {
-
-	}
-
-	public class User {
-
-	}
-	
 	public class CustomScope extends StructuredTaskScope<String>{
 		public String getBestString() {
 			return "";
 		}
 	}
+	protected boolean soundSame(String wordA, String wordB){
+		String scorew1 =Soundex.getGode(wordA);
+		String scorew2 =Soundex.getGode(wordB);
+
+		return scorew1.equals(scorew2);
+	}
+	protected boolean soundSimilar(String wordA, String wordB){
+		String scorew1 =Soundex.getGode(wordA);
+		String scorew2 =Soundex.getGode(wordB);
+		int w1=Integer.valueOf(scorew1.substring(1,scorew1.length()));
+		int w2=Integer.valueOf(scorew2.substring(1,scorew2.length()));
+		return w1==w2;
+	}
+
+	protected boolean soundSimilarWithTolerance(String wordA, String wordB, int tolerance){
+		String scorew1 =Soundex.getGode(wordA);
+		String scorew2 =Soundex.getGode(wordB);
+		int w1=Integer.valueOf(scorew1.substring(1,scorew1.length()));
+		int w2=Integer.valueOf(scorew2.substring(1,scorew2.length()));
+
+		return !(Math.abs( w1-w2) > tolerance);
+	}
+
 
 	protected String getStringInAConvolutedWay() throws InterruptedException, ExecutionException {
 		try (var scope = new StructuredTaskScope.ShutdownOnSuccess<String>()) {
--- src/test/java/com/eon/jdk19loom/StructuredConcurrencyExampleTest.java
+++ src/test/java/com/eon/jdk19loom/StructuredConcurrencyExampleTest.java
@@ -13,4 +13,30 @@ class StructuredConcurrencyExampleTest {
 		String hello = new StructuredConcurrencyExample().getStringInAConvolutedWay();
 		System.out.println(hello+", world");
 	}
+	@Test
+	void testSoundexDemo(){
+		assertTrue(new StructuredConcurrencyExample().soundSame("byte","bite"));
+		assertTrue(new StructuredConcurrencyExample().soundSame("reid","reed"));
+		assertTrue(new StructuredConcurrencyExample().soundSame("jik","jick"));
+	}
+
+	@Test
+	void testSoundexDemo2(){
+		assertTrue(new StructuredConcurrencyExample().soundSimilar("byte","bite"));
+		assertTrue(new StructuredConcurrencyExample().soundSimilar("reid","reed"));
+		assertTrue(new StructuredConcurrencyExample().soundSimilar("jik","jick"));
+		assertTrue(new StructuredConcurrencyExample().soundSimilar("home","ome"));
+	}
+
+	@Test
+	void testSoundexDemo4(){
+		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("byte","bite",0));
+		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("reid","reed",0));
+		assertTrue(new StructuredConcurrencyExample().soundSimilarWithTolerance("jik","jick",0));
+		assertFalse(new StructuredConcurrencyExample().soundSimilarWithTolerance("home","ute",10));
+	}
+
+
+
+
 }
